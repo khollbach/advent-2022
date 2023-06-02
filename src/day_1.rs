4da2ee6653685b420a93a2d1099543fb3fde469d
Kevan Hollbach <khollbach@gmail.com>
2023-06-02 16:01:44 -0700

    1-2

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,7 +1,11 @@
+use std::cmp::Reverse;
+
+use itertools::Itertools;
+
 use crate::input;
 
 #[test]
-fn main() {
+fn part_1() {
     let input = input!(1);
 
     let max_sum: u32 = input
@@ -17,4 +21,25 @@ fn main() {
         .unwrap();
 
     dbg!(max_sum);
+}
+
+#[test]
+fn part_2() {
+    let input = input!(1);
+
+    let three_largest: u32 = input
+        .split("\n\n")
+        .map(|group| {
+            let nums = group.lines().map(|line| {
+                let n: u32 = line.parse().unwrap();
+                n
+            });
+            let sum: u32 = nums.sum();
+            sum
+        })
+        .sorted_by_key(|&n| Reverse(n))
+        .take(3)
+        .sum();
+
+    dbg!(three_largest);
 }
